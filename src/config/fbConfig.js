e7907efed2fdeb9dcb6e2148d4e570a8964888d9
Ariane Holford <aribomop@gmail.com>
2020-09-04 15:12:02 +0100

    set up firebase firestore

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export default firebase;
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,10 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import ProductList from "../productdata";
+import firebase from "../config/fbConfig";
 
 export const ProductContext = createContext();
 
 const ProductContextProvider = (props) => {
-	const [products, setProducts] = useState(ProductList);
+	// const [products, setProducts] = useState(ProductList);
+
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const db = firebase.firestore();
+		return db.collection("products").onSnapshot((snapshot) => {
+			const productData = [];
+			snapshot.forEach((product) =>
+				productData.push({ ...product.data(), id: product.id })
+			);
+			setProducts(productData);
+		});
+	}, []);
 
 	return (
 		<ProductContext.Provider value={{ products }}>
