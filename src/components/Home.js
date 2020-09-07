88fd5b23f30c3b55753b1f928f21bb484addc4ce
Ariane Holford <aribomop@gmail.com>
2020-09-07 17:15:25 +0100

    can download an image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@material-ui/core";
+import firebase from "firebase";
 
 const Home = () => {
+	const [imageUrl, setImageUrl] = useState("");
+
+	const getImage = () => {
+		var storage = firebase.storage();
+		var gsReference = storage.refFromURL(
+			"gs://shoe-shop-f5df2.appspot.com/nike-air-max.jpg"
+		);
+
+		gsReference
+			.getDownloadURL()
+			.then((url) => {
+				setImageUrl(url);
+			})
+			.catch((err) => console.log(err));
+	};
+
+	getImage();
+
 	return (
-		<div>
-			<h1>home</h1>
-		</div>
+		<>
+			<Box>
+				<img src={imageUrl} alt="" />
+			</Box>
+		</>
 	);
 };
 
